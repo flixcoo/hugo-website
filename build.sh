e856f8f50af89843e0145b9081c4e60fb4f52050
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-09-26 00:09:07 +0200

    Implemented build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+echo "Updating the repository..."
+git pull
+
+echo "Removing old public files"
+rm -rf public/
+
+echo "Creating new public files"
+hugo
+
+echo "Removing old public files"
+rm -rf /var/www/felixkirchner.de/public/
+
+echo "Copying new public files"
+cp -r public/ /var/www/felixkirchner.de/public/
+
+echo "Restarting nginx..."
+sudo systemctl restart nginx
+
+echo "Done!"
