2826a289e130539c2a00c20113f1e967e14dc503
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-01-08 09:23:13 +0100

    Updated building script

--- build.sh
+++ build.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-echo "Updating the repository..."
+echo "=== Updating the repository..."
 git pull
 
-echo "Removing old public files"
+echo "=== Removing old public files"
 rm -rf public/
 
-echo "Creating new public files"
+echo "=== Creating new public files"
 hugo
 
-echo "Removing old public files"
-rm -rf /var/www/felixkirchner.de/public/
-
-echo "Copying new public files"
-cp -r public/ /var/www/felixkirchner.de/public/
-
-echo "Restarting nginx..."
-sudo systemctl restart nginx
-
-echo "Done!"
+echo "=== Done!"
